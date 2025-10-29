1c9e8203c9f6b7abd48166ab96b494b2211a8c24
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2025-10-29 18:05:19 +0100

    feat: Mocked initial UI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // mod api;
+mod config;
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -10,11 +11,15 @@ use std::time::Duration;
 use livesplit_core::{Run, Segment, Timer, TimerPhase};
 use tracing_subscriber;
 
+use adw::prelude::*;
+use adw::{Application, ApplicationWindow};
 use glib::ControlFlow::Continue;
 use gtk4::prelude::*;
-use gtk4::{Application, ApplicationWindow, Box as GtkBox, Button, Label, Orientation};
+use gtk4::{gdk::Display, Box as GtkBox, Builder, Button, CssProvider, Label, Orientation};
 
 fn main() {
+    adw::init().expect("Failed to initialize libadwaita");
+
     let app = Application::builder()
         .application_id("org.UnixSplit.unixplit-beta")
         .build();
@@ -44,94 +49,35 @@ impl UnixSplit {
         }
     }
 
+    fn load_css() {
+        let provider = CssProvider::new();
+        provider.load_from_path("data/css/livesplit-gtk.css");
+
+        let display = Display::default().expect("Could not connect to a display");
+        gtk4::style_context_add_provider_for_display(
+            &display,
+            &provider,
+            gtk4::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
+    }
+
     fn build_ui(&mut self, app: &Application) {
+        Self::load_css();
+
+        let builder = Builder::from_file("data/ui/livesplit-gtk.ui");
+
+        let clamp: adw::Clamp = builder
+            .object("livesplit-gtk")
+            .expect("Couldn't get main widget");
+
         let window = ApplicationWindow::builder()
             .application(app)
-            .title("LiveSplit GTK Starter")
+            .title("LiveSplit GTK")
+            .content(&clamp)
             .default_width(400)
-            .default_height(120)
+            .default_height(600)
             .build();
 
-        let vbox = GtkBox::new(Orientation::Vertical, 8);
-        vbox.set_margin_top(12);
-        vbox.set_margin_bottom(12);
-        vbox.set_margin_start(12);
-        vbox.set_margin_end(12);
-
-        let time_label = Label::new(Some("Not running"));
-        time_label.set_wrap(true);
-        vbox.append(&time_label);
-
-        let hbox = GtkBox::new(Orientation::Horizontal, 6);
-
-        let start_button = Button::with_label("Start");
-        let split_button = Button::with_label("Split");
-        let reset_button = Button::with_label("Reset");
-
-        hbox.append(&start_button);
-        hbox.append(&split_button);
-        hbox.append(&reset_button);
-
-        vbox.append(&hbox);
-
-        window.set_child(Some(&vbox));
-
-        // Clone handles for closures
-        let timer_for_start = self.timer.clone();
-        start_button.connect_clicked(move |_| {
-            let mut t = timer_for_start.borrow_mut();
-            if t.current_phase() == TimerPhase::NotRunning {
-                t.start();
-            }
-        });
-
-        let timer_for_split = self.timer.clone();
-        split_button.connect_clicked(move |_| {
-            let mut t = timer_for_split.borrow_mut();
-            if t.current_phase() == TimerPhase::Running {
-                t.split();
-            }
-        });
-
-        let timer_for_reset = self.timer.clone();
-        reset_button.connect_clicked(move |_| {
-            let mut t = timer_for_reset.borrow_mut();
-            t.reset(true);
-        });
-
-        let time_label_updater = time_label.clone();
-        let timer_for_loop = self.timer.clone();
-
-        // GLib timeout to update UI on the main thread.
-        glib::timeout_add_local(Duration::from_millis(16), move || {
-            let t = timer_for_loop.borrow_mut();
-            match t.current_phase() {
-                TimerPhase::NotRunning => {
-                    time_label_updater.set_text("Not running");
-                }
-                TimerPhase::Running => {
-                    let time = t.current_attempt_duration();
-                    let hours = time.total_seconds() as i32 / 3600;
-                    let minutes = time.total_seconds() as i32 / 60 % 60;
-                    let seconds = time.total_seconds() as i32 % 60;
-                    let milliseconds = time.total_milliseconds() as i32 % 1000;
-                    let s = format!(
-                        "{:02}:{:02}:{:02}.{:03}",
-                        hours, minutes, seconds, milliseconds
-                    );
-                    time_label_updater.set_text(&s);
-                }
-                TimerPhase::Ended => {
-                    time_label_updater.set_text("Run ended");
-                }
-                TimerPhase::Paused => {
-                    time_label_updater.set_text("Paused");
-                }
-            }
-
-            Continue
-        });
-
-        window.show();
+        window.present();
     }
 }
